2d9c33abc6ba5a680ea49a742c06a8987808258a
oleg-ivanov-nabusteam <oleg.ivanov.nabusteam@gmail.com>
2015-03-14 15:55:08 +0200

    added test_task.js

---
+++ test_task/test_task.js
@@ -0,0 +1,12 @@
+var test_task = function() {
+
+	var handleGettingAmountOfUsersOnThePlatform = function() {
+		console.log('Getting amount of users on the platform');
+	};
+
+	return {
+		init: function() {
+			handleGettingAmountOfUsersOnThePlatform();
+		}
+	};
+}();
