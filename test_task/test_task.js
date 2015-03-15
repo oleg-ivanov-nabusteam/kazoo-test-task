78570fea89380cf5bfad5ac5e0d9fd359c58008f
oleg-ivanov-nabusteam <oleg.ivanov.nabusteam@gmail.com>
2015-03-15 07:35:18 +0200

    list of realms in use

--- test_task/test_task.js
+++ test_task/test_task.js
@@ -5,6 +5,16 @@ var test_task = function() {
 	var accountName = 'oleg';
 	var accountId = '208064f16b0a1aa4791028dfd572ef97';
 	
+	var getAccountsDescendants = function(authToken) {
+		
+		return $.ajax({
+			type: 'GET',
+			url: 'http://dev.qualivoip.nl:8000/v1/accounts/' + accountId + '/descendants?auth_token=' + authToken
+		}).pipe(function(data) {
+			return data.data;
+		});
+	};
+	
 	var getAccounts = function(authToken) {
 		
 		return $.ajax({
@@ -48,10 +58,21 @@ var test_task = function() {
 			getAuthToken().pipe(getAccounts).done(function(data) {
 				var accountNumber = data.descendants_count + 1;
 				$('#ws-content').empty().append('<h3>Amount of users on the platform: ' + accountNumber + '<h3>');
-			})
+			});
 		});
 		
 		$('#list-of-realms-button').live('click', function () {
+			var listOfRealmsInUse = '<h2>List of realms in use:' + '</h2>';
+			
+			$.when(getAuthToken().pipe(getAccounts), getAuthToken().pipe(getAccountsDescendants)).then(function(account, accounts) {
+				listOfRealmsInUse += '<h3>' + account.realm + '</h3>'
+				
+				$.each(accounts, function(index, acc) {
+					listOfRealmsInUse += '<h3>' + acc.realm + '</h3>';
+				});
+				
+				$('#ws-content').empty().append(listOfRealmsInUse);
+			});
 		});
 	};
 
