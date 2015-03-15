1a085bc3fe639cd03753f2a549ba92b77868dc82
oleg-ivanov-nabusteam <oleg.ivanov.nabusteam@gmail.com>
2015-03-15 04:14:09 +0200

    rewrite code using deffered

--- test_task/test_task.js
+++ test_task/test_task.js
@@ -4,24 +4,19 @@ var test_task = function() {
 	var accountName = 'oleg';
 	var accountId = '208064f16b0a1aa4791028dfd572ef97';
 	
-	var outputAmountOfUsers = function(amountOfUsers){
+	var getAccountsDescendants = function(authToken) {
 		
-		$('#ws-content').empty().append('<h2>Amount of users on the platform: ' + amountOfUsers + '<h2>');
-	};
-	
-	var getAmountOfUsersOnThePlatform = function(authToken, callback) {
-		$.ajax({
+		return $.ajax({
 			type: 'GET',
-			url: 'http://dev.qualivoip.nl:8000/v1/accounts/' + accountId + '/descendants?auth_token=' + authToken,
-			success: function(data) {
-				callback(data.data.length);
-			}
+			url: 'http://dev.qualivoip.nl:8000/v1/accounts/' + accountId + '/descendants?auth_token=' + authToken
+		}).pipe(function(data) {
+			return data.data;
 		});
 	};
 	
-	var getAuthToken = function(callback) {
+	var getAuthToken = function() {
 	
-		$.ajax({
+		return $.ajax({
 			type: 'PUT',
 			url: 'http://dev.qualivoip.nl:8000/v1/user_auth',
 			contentType: 'application/json',
@@ -30,27 +25,24 @@ var test_task = function() {
 					credentials: credentials,
 					account_name: accountName
 				}
-			}),
-			success: function(data) {
-				callback(data.auth_token);
-			}
+			})
+		}).pipe(function(data) {
+			return data.auth_token;
 		});
 	};
-
-	var handleGettingAmountOfUsersOnThePlatform = function() {
+	
+	var handleInit = function() {
 		
 		$('#amount-of-users-button').live('click', function () {
-			getAuthToken(function(authToken) {
-				getAmountOfUsersOnThePlatform(authToken, function(amountOfUsers) {
-					outputAmountOfUsers(amountOfUsers);
-				});
-			});
+			getAuthToken().pipe(getAccountsDescendants).done(function(accounts) {
+				$('#ws-content').empty().append('<h3>Amount of users on the platform: ' + accounts.length + '<h3>');
+			})
 		});
 	};
 
 	return {
 		init: function() {
-			handleGettingAmountOfUsersOnThePlatform();
+			handleInit();
 		}
 	};
 }();
